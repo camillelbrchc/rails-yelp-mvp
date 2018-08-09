a22ab352f71926315fe2c13661f65d67d67c9de2
Camille Le Bruchec <Camille.lebruchec@gmail.com>
2018-08-09 08:34:02 +0200

    reviews improved but still failling

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -5,10 +5,20 @@ class ReviewsController < ApplicationController
     @review = Review.new
   end
 
+   def create
+    @review = Review.new(review_params)
+    @restaurant = Restaurant.find(params[:restaurant_id])
+
+    @review.restaurant = @restaurant
+
+    @review.save
+    redirect_to restaurant_path(@restaurant)
+  end
+
   private
 
   def review_params
-    params.require(:review).permit(:content)
+    params.require(:review).permit(:content, :rating)
   end
 
 end
